a0098431aa9a1c04c18b17d3e8c061827b161b0c
Murzin.V <murzinvit@gmail.com>
2021-08-25 05:25:12 +0000

    Modify start_ & requirements.yml - Bversion 1.0.1

--- ansible_elk_stack/start_centos.sh
+++ ansible_elk_stack/start_centos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm centos
-docker run -d -p 9200:9200 -p 5601:5601 -p 9600:9600 --name centos centos:latest sleep 60000000
+docker run -d -p 9200:9200 -p 5601:5601 -p 9600-9700:9600-9700 -p 5044:5044 --name centos centos:latest sleep 60000000
 docker ps
